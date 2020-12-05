4beb186e85d020d891f1a765c35e7dd2b056a0c4
darkbot <darkbotbbl@gmail.com>
2020-12-05 11:25:12 +0000

    Moved the simple note app to a vuex implementation using the vuex.js script

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 // vuex store 
+
+// state
 const state = {
     notes: [],
     timestamps: [],
 }
 
-const mutation = {
-    ADD_NOTE() {
-
+// mutations
+const mutations = {
+    ADD_NOTE(state, payload) {
+		state.notes.push(payload);
     },
-    ADD_TIMESTAMP() {
-
+    ADD_TIMESTAMP(state, payload) {
+		state.timestamps.push(payload);
     },
 }
+
+// actions 
+const actions = {
+	addNote(context, payload) {
+		context.commit('ADD_NOTE', payload);
+	},
+	addTimeStamp(context, payload) {
+		context.commit('ADD_TIMESTAMP', payload);
+	}
+}
+
+// getters
+const getters = {
+	getNotes: state => state.notes,
+	getTimeStamps: state => state.timestamps,
+	getNotesCount: state => state.notes.length,
+}
+
+const store = new Vuex.Store({
+	state,
+	getters,
+	mutations,
+	actions,
+})
+
 
 // event component
 const inputComponent = {
@@ -32,7 +60,9 @@ const inputComponent = {
     },
     methods: {
         monitorEnterKey() {
-
+			this.$store.dispatch('addNote', this.input);
+			this.$store.dispatch('addTimeStamp', new Date().toLocaleString());
+			this.input = '';
         }
     },
 }
@@ -40,13 +70,29 @@ const inputComponent = {
 // notes counter component
 const notesCounterComponent = {
     template: ` <p>Number of Notes: <span>{{ number_of_notes }}</span></p>`,
-    data() {
-        return {
-            number_of_notes: 0,
-        }
-    },
+	computed: {
+		number_of_notes(){
+			this.$store.getters.getNotesCount
+		}
+	}
 }
 
 new Vue({
-   
+	el: "#app",
+	store,
+	data: {
+		placeholder: "Enter a Note!!!",
+	},
+	components: {
+		inputComponent,
+		notesCounterComponent,
+	},
+	computed: {
+		notes() {
+			return this.$store.getters.getNotes;
+		},
+		timestamps() {
+			return this.$store.getters.getTimeStamps;
+		}
+	}
 })
